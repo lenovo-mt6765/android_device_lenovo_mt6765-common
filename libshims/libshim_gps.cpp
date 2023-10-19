fb0c1b235ca4bcd9ff0f704c65b8fdf5c8217284
R0rt1z2 <me@r0rt1z2.com>
2023-10-19 21:22:43 +0200

    mt6765-common: libshims: Shim missing libmnl symbols
     * dlopen failed: cannot locate symbol "mtk_gps_sys_agps_disaptcher_callback" referenced by "/vendor/lib/libmnl.so"...
    
    Signed-off-by: R0rt1z2 <me@r0rt1z2.com>
    Change-Id: I2aebf5704330231ad9d179a8ced30ad25c33de1b

--- libshims/libshim_gps.cpp
+++ libshims/libshim_gps.cpp
@@ -3,6 +3,25 @@
 #include <dlfcn.h>
 #include <string.h>
 
+//
+// Return value for most GPS APIs.
+//
+#define MTK_GPS_SUCCESS    (0)
+
+//
+// This function is part of the main GPS controller.
+// For some reason it seems to be used but not implemented.
+// Provide a dummy stub that returns success regardless of
+// the parameters that are passed on.
+//
+extern "C" int32_t mtk_gps_sys_agps_disaptcher_callback(uint16_t type, uint16_t length, char *data) {
+    // This is supposed to be a callback from agent to dispatcher.
+    // The third parameter holds the checksum(1) of 'PMTK_FS_REQ_MEAS'.
+    // We don't really care about the dispatcher. Return success and
+    // pretend everything's fine.
+    return MTK_GPS_SUCCESS;
+}
+
 extern "C" float mtk_flp_controller_get_current_acc() {
     return 1;
 }
