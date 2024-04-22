7bd1aff16733fedb750a98b30c292537ae181152
jmpfbmx <jmpf.bmx@gmail.com>
2024-04-22 17:17:37 +0200

    mt6765-common: Add arch, bootloader and platform flags
    Signed-off-by: jmpfbmx <jmpf.bmx@gmail.com>
    Change-Id: I06932663bba7391258d1b234bc3f62a9b85917e5

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -6,5 +6,27 @@
 
 COMMON_PATH := device/lenovo/mt6765-common
 
+# Architecture
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-a
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 :=
+TARGET_CPU_VARIANT := generic
+TARGET_CPU_VARIANT_RUNTIME := cortex-a53
+
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv8-a
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := generic
+TARGET_2ND_CPU_VARIANT_RUNTIME := cortex-a53
+
+# Bootloader
+TARGET_BOOTLOADER_BOARD_NAME := mt6765
+TARGET_NO_BOOTLOADER := true
+
+# Platform
+TARGET_BOARD_PLATFORM := mt6765
+
 # Inherit the proprietary files
 include vendor/lenovo/mt6765-common/BoardConfigVendor.mk
