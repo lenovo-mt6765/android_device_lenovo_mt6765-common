61d04aa68b7d4e93a210a91b3b77303b73a209ce
jmpfbmx <jmpf.bmx@gmail.com>
2024-04-22 18:14:59 +0200

    mt6765-common: extract-files: Link libutinterface_md.so to libutinterface_custom_md.so
    Signed-off-by: jmpfbmx <jmpf.bmx@gmail.com>
    Change-Id: Ib6cc81dc817677b50f4d71ee81000de7356a0bc6

--- extract-files.sh
+++ extract-files.sh
@@ -60,6 +60,9 @@ fi
 
 function blob_fixup() {
     case "${1}" in
+        vendor/lib*/libutinterface_custom_md.so)
+            "${PATCHELF}" --add-needed libutinterface_md.so "${2}"
+            ;;
         vendor/lib/libgeofence.so)
             "${PATCHELF}" --add-needed libshim_gps.so "${2}"
             ;;
