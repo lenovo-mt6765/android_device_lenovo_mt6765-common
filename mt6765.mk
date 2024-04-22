707b5992c349fde27435c2606faceba8efd8048b
jmpfbmx <jmpf.bmx@gmail.com>
2024-04-22 17:35:32 +0200

    mt6765-common: Import GPS config
    Signed-off-by: jmpfbmx <jmpf.bmx@gmail.com>
    Change-Id: I34cf14da2f3967ed2529b0f44864feebf8526a65

--- mt6765.mk
+++ mt6765.mk
@@ -27,6 +27,10 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml
 
+# GPS
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/agps_profiles_conf2.xml:$(TARGET_COPY_OUT_VENDOR)/etc/agps_profiles_conf2.xml
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
