5b3f93d35f33ad9c7b05e48c93dbbb7f601f6ae9
Christian Oder <myself5@carbonrom.org>
2024-04-22 17:24:47 +0200

    mt6765-common: Build disabled VBMeta image
    Change-Id: I381feef8f6fefc8449ca50d85d704b67bcc8a77e

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -69,5 +69,11 @@ TARGET_USERIMAGES_USE_EXT4 := true
 # Vendor SPL
 VENDOR_SECURITY_PATCH := 2022-05-05
 
+# Verified Boot
+BOARD_AVB_ENABLE := true
+BOARD_AVB_ROLLBACK_INDEX := $(PLATFORM_SECURITY_PATCH_TIMESTAMP)
+BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --set_hashtree_disabled_flag
+BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 2
+
 # Inherit the proprietary files
 include vendor/lenovo/mt6765-common/BoardConfigVendor.mk
