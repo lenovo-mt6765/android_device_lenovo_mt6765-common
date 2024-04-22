32d9e0651a74aa4f8e443989a91a43bfac750687
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2024-04-22 18:04:05 +0200

    mt6765-common: Be sure to build vndservicemanager
    * We need it for our blobs
    
    Change-Id: Ib757b97ee2c7d43fe606225366d729854ab987fc

--- mt6765.mk
+++ mt6765.mk
@@ -272,6 +272,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.1.vendor
 
+# Vendor service manager
+PRODUCT_PACKAGES += \
+    vndservicemanager
+
 # Vibrator
 PRODUCT_PACKAGES += \
     android.hardware.vibrator@1.0-impl \
