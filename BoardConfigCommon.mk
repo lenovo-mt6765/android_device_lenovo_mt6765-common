7d764a71bc0a131071f7305da1d45a6be429648a
jmpfbmx <jmpf.bmx@gmail.com>
2024-04-22 17:17:07 +0200

    mt6765-common: Initial skeleton common tree
    Signed-off-by: jmpfbmx <jmpf.bmx@gmail.com>
    Change-Id: Ib6ef40a667723cac3fd70d0c5d136089895a1317

---
+++ BoardConfigCommon.mk
@@ -0,0 +1,10 @@
+#
+# Copyright (C) 2024 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+COMMON_PATH := device/lenovo/mt6765-common
+
+# Inherit the proprietary files
+include vendor/lenovo/mt6765-common/BoardConfigVendor.mk
---
+++ mt6765.mk
@@ -0,0 +1,8 @@
+#
+# Copyright (C) 2024 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit the proprietary files
+$(call inherit-product, vendor/lenovo/mt6765-common/mt6765-common-vendor.mk)
