c2d55325937c92db8545714a6f1451d0e50cd736
Luca Stefani <luca.stefani.ge1@gmail.com>
2024-04-22 17:30:57 +0200

    mt6765-common: Enabled updateable APEX
    Change-Id: Iae46cbf48ae5fbfa100e83e8d6ce19f225102cf3

--- mt6765.mk
+++ mt6765.mk
@@ -4,5 +4,8 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Enable updating of APEXes
+$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
+
 # Inherit the proprietary files
 $(call inherit-product, vendor/lenovo/mt6765-common/mt6765-common-vendor.mk)
