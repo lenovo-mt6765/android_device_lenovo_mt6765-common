7992cbc827be46b869325f86e5c85327cb661ba1
jmpfbmx <jmpf.bmx@gmail.com>
2024-04-22 17:47:31 +0200

    mt6765-common: Build Bluetooth stack
    Signed-off-by: jmpfbmx <jmpf.bmx@gmail.com>
    Change-Id: I0a47dc7fcd1d0c99fb2e935984bb8c98657da698

--- mt6765.mk
+++ mt6765.mk
@@ -49,6 +49,12 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml
 
+# Bluetooth
+PRODUCT_PACKAGES += \
+    android.hardware.bluetooth.audio@2.0-impl \
+    android.hardware.bluetooth.a2dp@1.0.vendor \
+    android.hardware.bluetooth@1.0.vendor
+
 # GPS
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/agps_profiles_conf2.xml:$(TARGET_COPY_OUT_VENDOR)/etc/agps_profiles_conf2.xml
