97eefe1d5c47b6552f26d968f72a901e770a9823
jmpfbmx <jmpf.bmx@gmail.com>
2024-04-22 17:51:19 +0200

    mt6765-common: Build Graphics stack
    Signed-off-by: jmpfbmx <jmpf.bmx@gmail.com>
    Change-Id: Ic891deaa981350c5c7361d3b9d710cc7bef3f2c5

--- mt6765.mk
+++ mt6765.mk
@@ -79,6 +79,19 @@ PRODUCT_PACKAGES += \
     libdrmclearkeyplugin \
     libmockdrmcryptoplugin
 
+# Graphics
+PRODUCT_PACKAGES += \
+    android.hardware.graphics.allocator@2.0-service \
+    android.hardware.graphics.allocator@2.0-impl \
+    android.hardware.graphics.composer@2.1-service \
+    android.hardware.graphics.composer@2.1-impl \
+    android.hardware.graphics.mapper@2.0-impl-2.1 \
+    android.hardware.memtrack@1.0-service \
+    android.hardware.memtrack@1.0-impl \
+    disable_configstore \
+    libdrm \
+    libdrm.vendor
+
 # GPS
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/agps_profiles_conf2.xml:$(TARGET_COPY_OUT_VENDOR)/etc/agps_profiles_conf2.xml
