e85b141c66ba7a71b9133f41849d093eaf98ad9e
TheMalachite <eliasgheeraert@gmail.com>
2024-07-13 14:51:54 +0200

    mt6765-common: Add persist.log.tag properties overrides
    Change-Id: Ibd6a89cbc0fa17b36fff1a0759afb64705687d13

--- mt6765.mk
+++ mt6765.mk
@@ -219,6 +219,9 @@ PRODUCT_PACKAGES += \
     android.hardware.power@1.2.vendor \
     android.hardware.power@1.3.vendor
 
+# Properties
+include $(LOCAL_PATH)/vendor_logtag.mk
+
 # Public libraries
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
