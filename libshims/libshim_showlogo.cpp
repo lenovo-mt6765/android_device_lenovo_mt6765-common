fa9e074726139761c55891dfe440fd7f8d93c1dc
SamarV-121 <samarvispute121@gmail.com>
2024-07-14 02:44:55 +0200

    mt6765-common: Shim yet another missing libshowlogo symbol
    CANNOT LINK EXECUTABLE kpoc_charger: cannot locate symbol _ZN7android14SurfaceControl7releaseEv referenced by /system/lib/libshowlogo.so
    
    * Q : https://github.com/LineageOS/android_frameworks_native/blob/lineage-17.1/libs/gui/SurfaceControl.cpp#L82
    
    * R : LineageOS/android_frameworks_native@0e328f6
    
    Change-Id: I3ea190c8c5649aedb0c328b0567ff9f243422177

--- libshims/libshim_showlogo.cpp
+++ libshims/libshim_showlogo.cpp
@@ -11,4 +11,6 @@ namespace android {
   extern "C" void _ZN7android21SurfaceComposerClient13createSurfaceERKNS_7String8EjjijPNS_14SurfaceControlENS_13LayerMetadataE(const String8& name, uint32_t w, uint32_t h, PixelFormat format, uint32_t flags, SurfaceControl* parent, LayerMetadata metadata) {
 	  _ZN7android21SurfaceComposerClient13createSurfaceERKNS_7String8EjjijPNS_14SurfaceControlENS_13LayerMetadataEPj(name, w, h, format, flags, parent, metadata, nullptr);
 	}
+
+  extern "C" void _ZN7android14SurfaceControl7releaseEv() { }
 }
