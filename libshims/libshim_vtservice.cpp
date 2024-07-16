a322ddaf76ffaee3016688fb1a3e6fe8313df91a
bengris32 <bengris32@protonmail.ch>
2024-07-16 11:36:08 +0200

    mt6765-common: libshims: Fixup vtservice shim
     * All non-static class member functions (including constructors/destructors) carry a hidden this pointer. Add it.
    
     * AttributionSourceState now holds the pid and
     uid, create a new AttributionSourceState and set the uid and pid and pass it
     to new AudioTrack constructor.
    
     * _ZN7android10MediaMuxerC1EiNS0_12OutputFormatE is a class constructor, so
     call the new equivalent.
    
     * Only build 32bit library, since vtservice is 32bit
     on pre A12 IMS stack, and size_t on 32bit is unsigned int, while on 64 bit it
     is unsigned long, meaning that we can't link the shim against the 32bit and
     64bit lib since the symbol changes (well, we could with a compiler macro to
     use the correct symbol name for the 2 libs, but as stated before, there is no
     point).
    
    Signed-off-by: bengris32 <bengris32@protonmail.ch>
    Change-Id: I275fac7ddf63595f482ebef80d2116a322b6db60

--- libshims/libshim_vtservice.cpp
+++ libshims/libshim_vtservice.cpp
@@ -1,10 +1,32 @@
 #include <media/AudioTrack.h>
 #include <gui/IGraphicBufferProducer.h>
+#include <media/stagefright/MediaMuxer.h>
 
 using namespace android;
 
 extern "C" {
+    void _ZN7android10AudioTrackC1E19audio_stream_type_tj14audio_format_t20audio_channel_mask_tj20audio_output_flags_tPFviPvS5_ES5_i15audio_session_tNS0_13transfer_typeEPK20audio_offload_info_tRKNS_7content22AttributionSourceStateEPK18audio_attributes_tbfi(
+            void* thisptr,
+            audio_stream_type_t streamType,
+            uint32_t sampleRate,
+            audio_format_t format,
+            audio_channel_mask_t channelMask,
+            size_t frameCount,
+            audio_output_flags_t flags,
+            android::AudioTrack::callback_t cbf,
+            void* user,
+            int32_t notificationFrames,
+            audio_session_t sessionId,
+            android::AudioTrack::transfer_type transferType,
+            const audio_offload_info_t *offloadInfo,
+            const AttributionSourceState& attributionSource,
+            const audio_attributes_t* pAttributes,
+            bool doNotReconnect,
+            float maxRequiredSpeed,
+            audio_port_handle_t selectedDeviceId);
+
     void _ZN7android10AudioTrackC1E19audio_stream_type_tj14audio_format_tjj20audio_output_flags_tPFviPvS4_ES4_i15audio_session_tNS0_13transfer_typeEPK20audio_offload_info_tjiPK18audio_attributes_tbfi(
+            void* thisptr,
             audio_stream_type_t streamType,
             uint32_t sampleRate,
             audio_format_t format,
@@ -23,11 +45,15 @@ extern "C" {
             bool doNotReconnect,
             float maxRequiredSpeed,
             audio_port_handle_t selectedDeviceId) {
-        const AttributionSourceState& attributionSource = AttributionSourceState();
-        new android::AudioTrack(streamType, sampleRate, format, channelMask, frameCount, flags, cbf,
-                                user, notificationFrames, sessionId, transferType, offloadInfo, attributionSource,
-                                pAttributes, doNotReconnect, maxRequiredSpeed, selectedDeviceId);
-            }
+        AttributionSourceState* attributionSource = new AttributionSourceState();
+        attributionSource->uid = uid;
+        attributionSource->pid = pid;
+        _ZN7android10AudioTrackC1E19audio_stream_type_tj14audio_format_t20audio_channel_mask_tj20audio_output_flags_tPFviPvS5_ES5_i15audio_session_tNS0_13transfer_typeEPK20audio_offload_info_tRKNS_7content22AttributionSourceStateEPK18audio_attributes_tbfi(
+                            thisptr, streamType, sampleRate, format, channelMask, frameCount, flags, cbf,
+                            user, notificationFrames, sessionId, transferType, offloadInfo,
+                            *attributionSource, pAttributes,
+                            doNotReconnect, maxRequiredSpeed, selectedDeviceId);
+    }
 
     void _ZN7android11AudioSourceC1EPK18audio_attributes_tRKNS_8String16Ejjjjii28audio_microphone_direction_tf(const audio_attributes_t *attr, const String16 &opPackageName, uint32_t sampleRate, uint32_t channelCount, uint32_t outSampleRate, uid_t uid, pid_t pid, audio_port_handle_t selectedDeviceId, audio_microphone_direction_t selectedMicDirection, float selectedMicFieldDimension);
 
@@ -45,5 +71,9 @@ extern "C" {
             _ZN7android7SurfaceC1ERKNS_2spINS_22IGraphicBufferProducerEEEbRKNS1_INS_7IBinderEEE(thisptr, bufferProducer, controlledByApp, nullptr);
     }
 
-    int _ZN7android10MediaMuxerC1EiNS0_12OutputFormatE[6] = { 0, 1, 2, 3, 4, NULL };
+    void _ZN7android10MediaMuxerC1EiNS_14MediaMuxerBase12OutputFormatE(void* thisptr, int fd, android::MediaMuxer::OutputFormat format);
+
+    void _ZN7android10MediaMuxerC1EiNS0_12OutputFormatE(void* thisptr, int fd, android::MediaMuxer::OutputFormat format) {
+            _ZN7android10MediaMuxerC1EiNS_14MediaMuxerBase12OutputFormatE(thisptr, fd, format);
+    }
 }
